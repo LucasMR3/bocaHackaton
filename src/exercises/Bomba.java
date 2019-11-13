2c482429b668e0c6c75a0528b304ab2f9cd7c729
lucas_mr@live.com <lucas.ribeiro@sysmap.com.br>
2019-11-13 16:27:46 -0300

    little fix

--- src/exercises/Bomba.java
+++ src/exercises/Bomba.java
@@ -3,11 +3,8 @@ package exercises;
 import java.util.Scanner;
 
 public class Bomba {
-
 	public static void main(String[] args) {
-
 		int xa, ya, xb, yb, reps = 0, actualX, actualY = 0, alarms = 0;
-		int aux = 0;
 		Scanner sc = new Scanner(System.in);
 
 		xa = sc.nextInt();
@@ -16,9 +13,8 @@ public class Bomba {
 		yb = sc.nextInt();
 
 		reps = sc.nextInt();
-		int safe = 0;
 
-		while (aux != reps) {
+		for (int aux = 0; aux != reps; aux++) {
 			actualX = sc.nextInt();
 			actualY = sc.nextInt();
 
@@ -31,17 +27,14 @@ public class Bomba {
 					}
 				}
 			}
-			aux++;
 		}
-		
-		safe = reps - alarms;
+
+		int safe = reps - alarms;
 		if (safe == reps) {
 			System.out.println("SAFE");
 		} else {
 			System.out.printf("ALARM\n%d", alarms);
 		}
-
 		sc.close();
 	}
-
 }
